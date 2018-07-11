a7c58d4f7d4fadf672a2417aa089f24b9adf4713
Dan Fox <dfox@palantir.com>
2018-07-11 15:26:32 +0100

    10 failures left

--- conjure-java-verifier/src/test/java/com/palantir/conjure/java/compliance/AutoDeserializeTest.java
+++ conjure-java-verifier/src/test/java/com/palantir/conjure/java/compliance/AutoDeserializeTest.java
@@ -152,6 +152,7 @@ public class AutoDeserializeTest {
         ignores.put(EndpointName.of("receiveDateTimeAliasExample"), "null");
 
         // TODO(dfox): conjure-java should not coerce primitives from absent JSON
+        ignores.put(EndpointName.of("receiveBooleanExample"), "{}");
         ignores.put(EndpointName.of("receiveDoubleExample"), "{}");
         ignores.put(EndpointName.of("receiveIntegerExample"), "{}");
 
