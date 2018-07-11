d6e35d7966df25681952d8885e86233d4af1fb2a
Dan Sanduleac <dsanduleac@palantir.com>
2018-07-11 03:35:18 +0200

    (hacky) logic to wait on server to be up

--- conjure-java-verifier/src/test/java/com/palantir/conjure/java/compliance/ServerRule.java
+++ conjure-java-verifier/src/test/java/com/palantir/conjure/java/compliance/ServerRule.java
@@ -9,14 +9,23 @@ import com.palantir.remoting.api.config.ssl.SslConfiguration;
 import com.palantir.remoting3.clients.ClientConfiguration;
 import com.palantir.remoting3.clients.ClientConfigurations;
 import com.palantir.remoting3.config.ssl.SslSocketFactories;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.lang.ProcessBuilder.Redirect;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Paths;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 import javax.net.ssl.SSLSocketFactory;
 import javax.net.ssl.X509TrustManager;
 import org.junit.rules.ExternalResource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public final class ServerRule extends ExternalResource {
+    private static final Logger log = LoggerFactory.getLogger(ServerRule.class);
+
     private static final SslConfiguration TRUST_STORE_CONFIGURATION = new SslConfiguration.Builder()
             .trustStorePath(Paths.get("../conjure-java-core/var/security/truststore.jks"))
             .build();
@@ -37,10 +46,35 @@ public final class ServerRule extends ExternalResource {
         ProcessBuilder processBuilder =
                 new ProcessBuilder("build/verification-server/server", "build/test-cases/test-cases.json")
                         .redirectErrorStream(true)
-                        .redirectOutput(Redirect.INHERIT);
+                        .redirectOutput(Redirect.PIPE);
         // TODO(dsanduleac): set these as defaults
         processBuilder.environment().put("RUST_LOG", "conjure_verification_server=info,conjure_verification_http=info");
+
+        CountDownLatch latch = new CountDownLatch(1);
+        Thread thread = new Thread(() -> {
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream(),
+                    StandardCharsets.UTF_8))) {
+                while (true) {
+                    String line = reader.readLine();
+                    if (line == null) {
+                        break;
+                    }
+                    // TODO(dsanduleac) - super hacky
+                    // should have logic to derive port from the server's output in a structured way
+                    if (line.contains("Listening on")) {
+                        latch.countDown();
+                    }
+                    System.out.println(line);
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        thread.setDaemon(true);
         process = processBuilder.start();
+        thread.start();
+        log.info("Waiting for server to start up");
+        latch.await(10, TimeUnit.SECONDS);
     }
 
     @Override
