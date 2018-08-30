3ec4616462615955ad43c8097c9ff9bc98b53670
Dan Sanduleac <dsanduleac@palantir.com>
2018-08-30 19:34:07 +0100

    and don't import Conjure anymore

--- conjure-java-core/src/test/java/com/palantir/conjure/java/JerseyServiceEteTest.java
+++ conjure-java-core/src/test/java/com/palantir/conjure/java/JerseyServiceEteTest.java
@@ -21,7 +21,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
-import com.palantir.conjure.defs.Conjure;
 import com.palantir.conjure.java.lib.SafeLong;
 import com.palantir.conjure.java.services.JerseyServiceGenerator;
 import com.palantir.conjure.spec.ConjureDefinition;
