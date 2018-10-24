f1a1a0fb2c1c7e288e61928289e5a730f355a4b6
Patrick Koenig <pkoenig10@gmail.com>
2018-10-24 07:00:55 -0700

    Add Accept header to Retrofit clients (#92)

--- conjure-java-core/src/integrationInput/java/com/palantir/product/EteServiceRetrofit.java
+++ conjure-java-core/src/integrationInput/java/com/palantir/product/EteServiceRetrofit.java
@@ -24,63 +24,63 @@ import retrofit2.http.Streaming;
 @Generated("com.palantir.conjure.java.services.Retrofit2ServiceGenerator")
 public interface EteServiceRetrofit {
     @GET("./base/string")
-    @Headers("hr-path-template: /base/string")
+    @Headers({"hr-path-template: /base/string", "Accept: application/json"})
     Call<String> string(@Header("Authorization") AuthHeader authHeader);
 
     @GET("./base/integer")
-    @Headers("hr-path-template: /base/integer")
+    @Headers({"hr-path-template: /base/integer", "Accept: application/json"})
     Call<Integer> integer(@Header("Authorization") AuthHeader authHeader);
 
     @GET("./base/double")
-    @Headers("hr-path-template: /base/double")
+    @Headers({"hr-path-template: /base/double", "Accept: application/json"})
     Call<Double> double_(@Header("Authorization") AuthHeader authHeader);
 
     @GET("./base/boolean")
-    @Headers("hr-path-template: /base/boolean")
+    @Headers({"hr-path-template: /base/boolean", "Accept: application/json"})
     Call<Boolean> boolean_(@Header("Authorization") AuthHeader authHeader);
 
     @GET("./base/safelong")
-    @Headers("hr-path-template: /base/safelong")
+    @Headers({"hr-path-template: /base/safelong", "Accept: application/json"})
     Call<SafeLong> safelong(@Header("Authorization") AuthHeader authHeader);
 
     @GET("./base/rid")
-    @Headers("hr-path-template: /base/rid")
+    @Headers({"hr-path-template: /base/rid", "Accept: application/json"})
     Call<ResourceIdentifier> rid(@Header("Authorization") AuthHeader authHeader);
 
     @GET("./base/bearertoken")
-    @Headers("hr-path-template: /base/bearertoken")
+    @Headers({"hr-path-template: /base/bearertoken", "Accept: application/json"})
     Call<BearerToken> bearertoken(@Header("Authorization") AuthHeader authHeader);
 
     @GET("./base/optionalString")
-    @Headers("hr-path-template: /base/optionalString")
+    @Headers({"hr-path-template: /base/optionalString", "Accept: application/json"})
     Call<Optional<String>> optionalString(@Header("Authorization") AuthHeader authHeader);
 
     @GET("./base/optionalEmpty")
-    @Headers("hr-path-template: /base/optionalEmpty")
+    @Headers({"hr-path-template: /base/optionalEmpty", "Accept: application/json"})
     Call<Optional<String>> optionalEmpty(@Header("Authorization") AuthHeader authHeader);
 
     @GET("./base/datetime")
-    @Headers("hr-path-template: /base/datetime")
+    @Headers({"hr-path-template: /base/datetime", "Accept: application/json"})
     Call<OffsetDateTime> datetime(@Header("Authorization") AuthHeader authHeader);
 
     @GET("./base/binary")
-    @Headers("hr-path-template: /base/binary")
+    @Headers({"hr-path-template: /base/binary", "Accept: application/octet-stream"})
     @Streaming
     Call<ResponseBody> binary(@Header("Authorization") AuthHeader authHeader);
 
     @POST("./base/notNullBody")
-    @Headers("hr-path-template: /base/notNullBody")
+    @Headers({"hr-path-template: /base/notNullBody", "Accept: application/json"})
     Call<StringAliasExample> notNullBody(
             @Header("Authorization") AuthHeader authHeader, @Body StringAliasExample notNullBody);
 
     @GET("./base/optionalAliasOne")
-    @Headers("hr-path-template: /base/optionalAliasOne")
+    @Headers({"hr-path-template: /base/optionalAliasOne", "Accept: application/json"})
     Call<StringAliasExample> optionalAliasOne(
             @Header("Authorization") AuthHeader authHeader,
             @Query("queryParamName") Optional<StringAliasExample> queryParamName);
 
     @GET("./base/aliasTwo")
-    @Headers("hr-path-template: /base/aliasTwo")
+    @Headers({"hr-path-template: /base/aliasTwo", "Accept: application/json"})
     Call<NestedStringAliasExample> aliasTwo(
             @Header("Authorization") AuthHeader authHeader,
             @Query("queryParamName") NestedStringAliasExample queryParamName);
