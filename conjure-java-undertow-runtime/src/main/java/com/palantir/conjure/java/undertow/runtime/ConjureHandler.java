b72774f31238b707f951140452d86c8c4335f2f5
Carter Kozak <ckozak@palantir.com>
2019-01-08 14:49:07 -0500

    Clarify Undertow ConjureHandler comment

--- conjure-java-undertow-runtime/src/main/java/com/palantir/conjure/java/undertow/runtime/ConjureHandler.java
+++ conjure-java-undertow-runtime/src/main/java/com/palantir/conjure/java/undertow/runtime/ConjureHandler.java
@@ -39,6 +39,7 @@ public final class ConjureHandler implements HttpHandler, RoutingRegistry {
             ImmutableList.<BiFunction<String, HttpHandler, HttpHandler>>of(
             // Allow the server to configure UndertowOptions.DECODE_URL = false to allow slashes in parameters.
             // Servers which do not configure DECODE_URL will still work properly except for encoded slash values.
+            // When DECODE_URL has not been disabled, the following handlers will no-op
             (endpoint, handler) -> new URLDecodingHandler(handler, "UTF-8"),
             (endpoint, handler) -> new PathParamDecodingHandler(handler),
             // It is vitally important to never run blocking operations on the initial IO thread otherwise
