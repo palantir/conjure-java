84cf8614161c3120f56f221a713d0de952959c24
iamdanfox <iamdanfox@gmail.com>
2019-02-01 16:59:11 +0000

    [fix] ConjureExceptionHandler understands QosExceptions (#219)
    ## Before this PR
    
    There are three kinds of QosException in conjure-java-runtime-api, but the ConjureExceptionHandler currently sends them all over the wire as 500 errors.
    
    (See the full list of available [exceptions here](https://github.com/palantir/conjure-java-runtime-api/tree/develop/errors/src/main/java/com/palantir/conjure/java/api/errors))
    
    ## After this PR
    
    The Undertow behaviour matches the old Jersey behaviour ([QosExceptionMapper](https://github.com/palantir/conjure-java-runtime/blob/develop/conjure-java-jersey-server/src/main/java/com/palantir/conjure/java/server/jersey/QosExceptionMapper.java))
    
    
    <!-- Describe at a high-level why this approach is better. -->
    
    <!-- Reference any existing GitHub issues, e.g. 'fixes #000' or 'relevant to #000' -->

--- conjure-java-undertow-runtime/src/main/java/com/palantir/conjure/java/undertow/runtime/ConjureExceptionHandler.java
+++ conjure-java-undertow-runtime/src/main/java/com/palantir/conjure/java/undertow/runtime/ConjureExceptionHandler.java
@@ -17,6 +17,7 @@
 package com.palantir.conjure.java.undertow.runtime;
 
 import com.palantir.conjure.java.api.errors.ErrorType;
+import com.palantir.conjure.java.api.errors.QosException;
 import com.palantir.conjure.java.api.errors.RemoteException;
 import com.palantir.conjure.java.api.errors.SerializableError;
 import com.palantir.conjure.java.api.errors.ServiceException;
@@ -25,8 +26,12 @@ import com.palantir.logsafe.SafeArg;
 import io.undertow.io.UndertowOutputStream;
 import io.undertow.server.HttpHandler;
 import io.undertow.server.HttpServerExchange;
+import io.undertow.util.Headers;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.time.temporal.ChronoUnit;
+import java.util.Optional;
+import java.util.function.Consumer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.xnio.IoUtils;
@@ -60,75 +65,105 @@ final class ConjureExceptionHandler implements HttpHandler {
         try {
             delegate.handleRequest(exchange);
         } catch (Throwable throwable) {
-            final SerializableError error;
-            final int statusCode;
-
             if (throwable instanceof ServiceException) {
-                ServiceException exception = (ServiceException) throwable;
-                statusCode = exception.getErrorType().httpErrorCode();
-                error = SerializableError.forException(exception);
-                log(exception);
+                serviceException(exchange, (ServiceException) throwable);
+            } else if (throwable instanceof QosException) {
+                qosException(exchange, (QosException) throwable);
             } else if (throwable instanceof RemoteException) {
-                // RemoteExceptions are thrown by Conjure clients to indicate a remote/service-side problem.
-                // We forward these exceptions, but change the ErrorType to INTERNAL, i.e., the problem is now
-                // considered internal to *this* service rather than the originating service. This means in particular
-                // that Conjure errors are defined only local to a given service and these error types don't
-                // propagate through other services.
-                RemoteException exception = (RemoteException) throwable;
-
-                // log at WARN instead of ERROR because although this indicates an issue in a remote server
-                log.warn("Encountered a remote exception. Mapping to an internal error before propagating",
-                        SafeArg.of("errorInstanceId", exception.getError().errorInstanceId()),
-                        SafeArg.of("errorName", exception.getError().errorName()),
-                        SafeArg.of("statusCode", exception.getStatus()),
-                        exception);
-
-                ErrorType errorType = ErrorType.INTERNAL;
-                statusCode = errorType.httpErrorCode();
-
-                // Override only the name and code of the error
-                error = SerializableError.builder()
-                        .from(exception.getError())
-                        .errorName(errorType.name())
-                        .errorCode(errorType.code().toString())
-                        .build();
+                remoteException(exchange, (RemoteException) throwable);
             } else if (throwable instanceof IllegalArgumentException) {
-                ServiceException exception = new ServiceException(ErrorType.INVALID_ARGUMENT, throwable);
-                error = SerializableError.forException(exception);
-                statusCode = exception.getErrorType().httpErrorCode();
-                log(exception);
+                illegalArgumentException(exchange, throwable);
             } else if (throwable instanceof FrameworkException) {
-                FrameworkException frameworkException = (FrameworkException) throwable;
-                statusCode = frameworkException.getStatusCode();
-                ErrorType errorType = statusCode / 100 == 4 ? ErrorType.INVALID_ARGUMENT : ErrorType.INTERNAL;
-                ServiceException exception = new ServiceException(errorType, frameworkException);
-                error = SerializableError.forException(exception);
-                log(exception);
+                frameworkException(exchange, (FrameworkException) throwable);
             } else if (throwable instanceof Error) {
                 throw (Error) throwable;
             } else {
                 ServiceException exception = new ServiceException(ErrorType.INTERNAL, throwable);
-                statusCode = exception.getErrorType().httpErrorCode();
-                error = SerializableError.forException(exception);
                 log(exception);
+                writeResponse(
+                        exchange,
+                        Optional.of(SerializableError.forException(exception)),
+                        exception.getErrorType().httpErrorCode());
             }
+        }
+    }
 
-            // Do not attempt to write the failure if data has already been written
-            if (!isResponseStarted(exchange)) {
-                exchange.setStatusCode(statusCode);
-                try {
-                    serializers.serialize(error, exchange);
-                } catch (IOException | RuntimeException e) {
-                    log.info("Failed to write error response", e);
+    private void serviceException(HttpServerExchange exchange, ServiceException exception) {
+        log(exception);
+        writeResponse(
+                exchange,
+                Optional.of(SerializableError.forException(exception)),
+                exception.getErrorType().httpErrorCode());
+    }
+
+    private void qosException(HttpServerExchange exchange, QosException exception) {
+        exception.accept(QOS_EXCEPTION_HEADERS).accept(exchange);
+        log.debug("Possible quality-of-service intervention", exception);
+        writeResponse(
+                exchange,
+                Optional.empty(),
+                exception.accept(QOS_EXCEPTION_STATUS_CODE));
+    }
+
+    // RemoteExceptions are thrown by Conjure clients to indicate a remote/service-side problem.
+    // We forward these exceptions, but change the ErrorType to INTERNAL, i.e., the problem is now
+    // considered internal to *this* service rather than the originating service. This means in particular
+    // that Conjure errors are defined only local to a given service and these error types don't
+    // propagate through other services.
+    private void remoteException(HttpServerExchange exchange, RemoteException exception) {
+        // log at WARN instead of ERROR because although this indicates an issue in a remote server
+        log.warn("Encountered a remote exception. Mapping to an internal error before propagating",
+                SafeArg.of("errorInstanceId", exception.getError().errorInstanceId()),
+                SafeArg.of("errorName", exception.getError().errorName()),
+                SafeArg.of("statusCode", exception.getStatus()),
+                exception);
+
+        ErrorType errorType = ErrorType.INTERNAL;
+        writeResponse(exchange,
+                // Override only the name and code of the error
+                Optional.of(SerializableError.builder()
+                        .from(exception.getError())
+                        .errorName(errorType.name())
+                        .errorCode(errorType.code().toString())
+                        .build()),
+                errorType.httpErrorCode());
+    }
+
+    private void illegalArgumentException(HttpServerExchange exchange, Throwable throwable) {
+        ServiceException exception = new ServiceException(ErrorType.INVALID_ARGUMENT, throwable);
+        log(exception);
+        writeResponse(
+                exchange,
+                Optional.of(SerializableError.forException(exception)),
+                exception.getErrorType().httpErrorCode());
+    }
+
+    private void frameworkException(HttpServerExchange exchange, FrameworkException frameworkException) {
+        int statusCode = frameworkException.getStatusCode();
+        ErrorType errorType = statusCode / 100 == 4 ? ErrorType.INVALID_ARGUMENT : ErrorType.INTERNAL;
+        ServiceException exception = new ServiceException(errorType, frameworkException);
+        log(exception);
+        writeResponse(exchange, Optional.of(SerializableError.forException(exception)), statusCode);
+    }
+
+    private void writeResponse(HttpServerExchange exchange, Optional<SerializableError> maybeBody, int statusCode) {
+        // Do not attempt to write the failure if data has already been written
+        if (!isResponseStarted(exchange)) {
+            exchange.setStatusCode(statusCode);
+            try {
+                if (maybeBody.isPresent()) {
+                    serializers.serialize(maybeBody.get(), exchange);
                 }
-            } else {
-                // This prevents the server from sending the final null chunk, alerting
-                // clients that the response was terminated prior to receiving full contents.
-                // Note that in the case of http/2 this does not close a connection, which
-                // would break other active requests, only resets the stream.
-                log.warn("Closing the connection to alert the client of an error");
-                IoUtils.safeClose(exchange.getConnection());
+            } catch (IOException | RuntimeException e) {
+                log.info("Failed to write error response", e);
             }
+        } else {
+            // This prevents the server from sending the final null chunk, alerting
+            // clients that the response was terminated prior to receiving full contents.
+            // Note that in the case of http/2 this does not close a connection, which
+            // would break other active requests, only resets the stream.
+            log.warn("Closing the connection to alert the client of an error");
+            IoUtils.safeClose(exchange.getConnection());
         }
     }
 
@@ -154,4 +189,45 @@ final class ConjureExceptionHandler implements HttpHandler {
                     SafeArg.of("errorInstanceId", exception.getErrorInstanceId()), exception);
         }
     }
+
+    private static final QosException.Visitor<Integer> QOS_EXCEPTION_STATUS_CODE = new QosException.Visitor<Integer>() {
+        @Override
+        public Integer visit(QosException.Throttle exception) {
+            return 429;
+        }
+
+        @Override
+        public Integer visit(QosException.RetryOther exception) {
+            return 308;
+        }
+
+        @Override
+        public Integer visit(QosException.Unavailable exception) {
+            return 503;
+        }
+    };
+
+    private static final QosException.Visitor<Consumer<HttpServerExchange>>
+            QOS_EXCEPTION_HEADERS = new QosException.Visitor<Consumer<HttpServerExchange>>() {
+                @Override
+                public Consumer<HttpServerExchange> visit(QosException.Throttle exception) {
+                    return exchange -> exception.getRetryAfter().ifPresent(duration -> {
+                        exchange.getResponseHeaders()
+                                .put(Headers.RETRY_AFTER, Long.toString(duration.get(ChronoUnit.SECONDS)));
+                    });
+                }
+
+                @Override
+                public Consumer<HttpServerExchange> visit(QosException.RetryOther exception) {
+                    return exchange -> exchange.getResponseHeaders()
+                            .put(Headers.LOCATION, exception.getRedirectTo().toString());
+                }
+
+                @Override
+                public Consumer<HttpServerExchange> visit(QosException.Unavailable exception) {
+                    return exchange -> {
+
+                    };
+                }
+            };
 }
