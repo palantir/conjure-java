3e2348e0f4ca5ed1ab9b8aec9bddda855d88b2d1
Carter Kozak <ckozak@ckozak.net>
2019-04-03 10:19:12 -0400

    [fix] conjure-undertow supports empty request body empty optionals (#319)
    conjure-undertow supports empty request body empty optional values

--- conjure-java-core/src/test/java/com/palantir/conjure/java/UndertowServiceEteTest.java
+++ conjure-java-core/src/test/java/com/palantir/conjure/java/UndertowServiceEteTest.java
@@ -343,6 +343,25 @@ public final class UndertowServiceEteTest extends TestBase {
     }
 
     @Test
+    public void testExternalImportOptionalEmptyBodyZeroLength_noContentType() throws IOException {
+        URL url = new URL("http://0.0.0.0:8080/test-example/api/base/external/optional-body");
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("POST");
+        con.setRequestProperty(HttpHeaders.AUTHORIZATION, AuthHeader.valueOf("authHeader").toString());
+        assertThat(con.getResponseCode()).isEqualTo(204);
+    }
+
+    @Test
+    public void testExternalImportOptionalEmptyBodyZeroLength_withContentType() throws IOException {
+        URL url = new URL("http://0.0.0.0:8080/test-example/api/base/external/optional-body");
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("POST");
+        con.setRequestProperty(HttpHeaders.CONTENT_TYPE, "application/json");
+        con.setRequestProperty(HttpHeaders.AUTHORIZATION, AuthHeader.valueOf("authHeader").toString());
+        assertThat(con.getResponseCode()).isEqualTo(204);
+    }
+
+    @Test
     public void testUnknownContentType() {
         assertThatThrownBy(() -> binaryClient.postBinary(AuthHeader.valueOf("authHeader"),
                 RequestBody.create(MediaType.parse("application/unsupported"), new byte[] {1, 2, 3})).execute())
--- conjure-java-undertow-runtime/src/main/java/com/palantir/conjure/java/undertow/runtime/ConjureBodySerDe.java
+++ conjure-java-undertow-runtime/src/main/java/com/palantir/conjure/java/undertow/runtime/ConjureBodySerDe.java
@@ -30,6 +30,7 @@ import io.undertow.util.HeaderValues;
 import io.undertow.util.Headers;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.PushbackInputStream;
 import java.util.List;
 
 /** Package private internal API. */
@@ -135,17 +136,44 @@ final class ConjureBodySerDe implements BodySerDe {
     private static final class EncodingDeserializerRegistry<T> implements Deserializer<T> {
 
         private final List<EncodingDeserializerContainer<T>> encodings;
+        private final boolean optionalType;
+        private final TypeMarker<T> marker;
 
         EncodingDeserializerRegistry(List<Encoding> encodings, TypeMarker<T> token) {
             this.encodings = encodings.stream()
                     .map(encoding -> new EncodingDeserializerContainer<>(encoding, token))
                     .collect(ImmutableList.toImmutableList());
+            this.optionalType = TypeMarkers.isOptional(token);
+            this.marker = token;
         }
 
         @Override
         public T deserialize(HttpServerExchange exchange) throws IOException {
+            if (optionalType && maybeEmptyBody(exchange)) {
+                return deserializeOptional(exchange);
+            }
+            return deserializeInternal(exchange, exchange.getInputStream());
+        }
+
+        private T deserializeOptional(HttpServerExchange exchange) throws IOException {
+            PushbackInputStream requestStream = new PushbackInputStream(exchange.getInputStream(), 1);
+            int read = requestStream.read();
+            if (read == -1) {
+                return TypeMarkers.getEmptyOptional(marker);
+            }
+            requestStream.unread(read);
+            return deserializeInternal(exchange, requestStream);
+        }
+
+        private T deserializeInternal(HttpServerExchange exchange, InputStream requestStream) throws IOException {
             EncodingDeserializerContainer<T> container = getRequestDeserializer(exchange);
-            return container.deserializer.deserialize(exchange.getInputStream());
+            return container.deserializer.deserialize(requestStream);
+        }
+
+        private static boolean maybeEmptyBody(HttpServerExchange exchange) {
+            // Content-Length maybe null if "Transfer-Encoding: chunked" is sent with a full body.
+            String contentLength = exchange.getRequestHeaders().getFirst(Headers.CONTENT_LENGTH);
+            return contentLength == null || "0".equals(contentLength);
         }
 
         /** Returns the {@link EncodingDeserializerContainer} to use to deserialize the request body. */
