c5e65e2acdb389276f247b82623debd4d679e949
Carter Kozak <ckozak@palantir.com>
2019-04-25 16:25:22 -0400

    Cancel the future if the client cancels a request

--- conjure-java-undertow-runtime/src/main/java/com/palantir/conjure/java/undertow/runtime/ConjureAsyncRequestProcessing.java
+++ conjure-java-undertow-runtime/src/main/java/com/palantir/conjure/java/undertow/runtime/ConjureAsyncRequestProcessing.java
@@ -25,7 +25,9 @@ import com.palantir.conjure.java.undertow.lib.AsyncRequestProcessing;
 import com.palantir.conjure.java.undertow.lib.ReturnValueWriter;
 import com.palantir.conjure.java.undertow.lib.Serializer;
 import com.palantir.logsafe.Preconditions;
+import io.undertow.server.ExchangeCompletionListener;
 import io.undertow.server.HttpServerExchange;
+import io.undertow.util.AttachmentKey;
 import java.io.IOException;
 import java.time.Duration;
 import java.util.concurrent.Executor;
@@ -36,6 +38,16 @@ import org.xnio.XnioExecutor;
 final class ConjureAsyncRequestProcessing implements AsyncRequestProcessing {
 
     private static final Executor DIRECT_EXECUTOR = MoreExecutors.directExecutor();
+    private static final AttachmentKey<ListenableFuture<?>> FUTURE = AttachmentKey.create(ListenableFuture.class);
+    // If the request is ended before the future has completed, cancel the future to signal that work
+    // should be stopped. This occurs when clients cancel requests or connections are closed.
+    private static final ExchangeCompletionListener COMPLETION_LISTENER = (exchange, nextListener) -> {
+        ListenableFuture<?> future = exchange.getAttachment(FUTURE);
+        if (future != null && !future.isDone()) {
+            future.cancel(false);
+        }
+        nextListener.proceed();
+    };
 
     private final Serializer<SerializableError> exceptionSerializer = ConjureExceptions.serializer();
 
@@ -57,26 +69,31 @@ final class ConjureAsyncRequestProcessing implements AsyncRequestProcessing {
         // TODO(ckozak): potential optimization: avoid both dispatching and scheduling a timeout if the future is
         // already complete.
 
+        // Attach data to the exchange in order to reuse a stateless listener
+        exchange.putAttachment(FUTURE, future);
+        // Cancel the future if the exchange is completed before the future is done
+        exchange.addExchangeCompleteListener(COMPLETION_LISTENER);
+
+        XnioExecutor.Key timeoutKey = exchange.getIoThread()
+                .executeAfter(() -> future.cancel(false), timeout.toMillis(), TimeUnit.MILLISECONDS);
+        future.addListener(timeoutKey::remove, DIRECT_EXECUTOR);
         // Dispatch the registration task. We're executing inside of root handler, so the dispatched task will
         // execute on this thread after the current handler completes execution. This allows us to avoid racing
         // registration with completion.
-        exchange.dispatch(() -> {
-            XnioExecutor.Key timeoutKey = exchange.getIoThread()
-                    .executeAfter(() -> future.cancel(false), timeout.toMillis(), TimeUnit.MILLISECONDS);
-            future.addListener(timeoutKey::remove, DIRECT_EXECUTOR);
-            Futures.addCallback(future, new FutureCallback<T>() {
-                @Override
-                public void onSuccess(@Nullable T result) {
-                    exchange.dispatch(new ConjureExceptionHandler(serverExchange ->
-                            returnValueWriter.write(result, exchange), exceptionSerializer));
-                }
+        // This leaves the exchange in a dispatched state preventing the calling Connectors.executeRootHandler
+        // from terminating this request while the future awaits completion.
+        exchange.dispatch(() -> Futures.addCallback(future, new FutureCallback<T>() {
+            @Override
+            public void onSuccess(@Nullable T result) {
+                exchange.dispatch(new ConjureExceptionHandler(serverExchange ->
+                        returnValueWriter.write(result, exchange), exceptionSerializer));
+            }
 
-                @Override
-                public void onFailure(Throwable throwable) {
-                    exchange.dispatch(new ConjureExceptionHandler(serverExchange -> ConjureExceptions.handle(
-                            serverExchange, exceptionSerializer, throwable), exceptionSerializer));
-                }
-            }, DIRECT_EXECUTOR);
-        });
+            @Override
+            public void onFailure(Throwable throwable) {
+                exchange.dispatch(new ConjureExceptionHandler(serverExchange -> ConjureExceptions.handle(
+                        serverExchange, exceptionSerializer, throwable), exceptionSerializer));
+            }
+        }, DIRECT_EXECUTOR));
     }
 }
