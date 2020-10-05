c91b06b2fea1af660b1e251397ba081713b2c135
Carter Kozak <ckozak@ckozak.net>
2020-10-05 09:16:06 -0400

    fix #573: Unions with type `unknown` compile as expected (#1103)
    Unions with type `unknown` compile as expected

---
+++ conjure-java-core/src/integrationInput/java/com/palantir/product/UnionWithUnknownString.java
@@ -0,0 +1,229 @@
+package com.palantir.product;
+
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.annotation.JsonTypeName;
+import com.fasterxml.jackson.annotation.JsonValue;
+import com.palantir.logsafe.Preconditions;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.function.Function;
+import javax.annotation.Generated;
+import javax.annotation.Nonnull;
+
+@Generated("com.palantir.conjure.java.types.UnionGenerator")
+public final class UnionWithUnknownString {
+    private final Base value;
+
+    @JsonCreator
+    private UnionWithUnknownString(Base value) {
+        this.value = value;
+    }
+
+    @JsonValue
+    private Base getValue() {
+        return value;
+    }
+
+    public static UnionWithUnknownString unknown_(String value) {
+        return new UnionWithUnknownString(new Unknown_Wrapper(value));
+    }
+
+    public <T> T accept(Visitor<T> visitor) {
+        return value.accept(visitor);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return this == other || (other instanceof UnionWithUnknownString && equalTo((UnionWithUnknownString) other));
+    }
+
+    private boolean equalTo(UnionWithUnknownString other) {
+        return this.value.equals(other.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(this.value);
+    }
+
+    @Override
+    public String toString() {
+        return "UnionWithUnknownString{value: " + value + '}';
+    }
+
+    public interface Visitor<T> {
+        T visitUnknown_(String value);
+
+        T visitUnknown(String unknownType);
+
+        static <T> Unknown_StageVisitorBuilder<T> builder() {
+            return new VisitorBuilder<T>();
+        }
+    }
+
+    private static final class VisitorBuilder<T>
+            implements Unknown_StageVisitorBuilder<T>, UnknownStageVisitorBuilder<T>, Completed_StageVisitorBuilder<T> {
+        private Function<String, T> unknown_Visitor;
+
+        private Function<String, T> unknownVisitor;
+
+        @Override
+        public UnknownStageVisitorBuilder<T> unknown_(@Nonnull Function<String, T> unknown_Visitor) {
+            Preconditions.checkNotNull(unknown_Visitor, "unknown_Visitor cannot be null");
+            this.unknown_Visitor = unknown_Visitor;
+            return this;
+        }
+
+        @Override
+        public Completed_StageVisitorBuilder<T> unknown(@Nonnull Function<String, T> unknownVisitor) {
+            Preconditions.checkNotNull(unknownVisitor, "unknownVisitor cannot be null");
+            this.unknownVisitor = unknownVisitor;
+            return this;
+        }
+
+        @Override
+        public Visitor<T> build() {
+            final Function<String, T> unknown_Visitor = this.unknown_Visitor;
+            final Function<String, T> unknownVisitor = this.unknownVisitor;
+            return new Visitor<T>() {
+                @Override
+                public T visitUnknown_(String value) {
+                    return unknown_Visitor.apply(value);
+                }
+
+                @Override
+                public T visitUnknown(String value) {
+                    return unknownVisitor.apply(value);
+                }
+            };
+        }
+    }
+
+    public interface Unknown_StageVisitorBuilder<T> {
+        UnknownStageVisitorBuilder<T> unknown_(@Nonnull Function<String, T> unknown_Visitor);
+    }
+
+    public interface UnknownStageVisitorBuilder<T> {
+        Completed_StageVisitorBuilder<T> unknown(@Nonnull Function<String, T> unknownVisitor);
+    }
+
+    public interface Completed_StageVisitorBuilder<T> {
+        Visitor<T> build();
+    }
+
+    @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "type", visible = true, defaultImpl = UnknownWrapper.class)
+    @JsonSubTypes(@JsonSubTypes.Type(Unknown_Wrapper.class))
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    private interface Base {
+        <T> T accept(Visitor<T> visitor);
+    }
+
+    @JsonTypeName("unknown")
+    private static final class Unknown_Wrapper implements Base {
+        private final String value;
+
+        @JsonCreator
+        private Unknown_Wrapper(@JsonProperty("unknown") @Nonnull String value) {
+            Preconditions.checkNotNull(value, "unknown_ cannot be null");
+            this.value = value;
+        }
+
+        @JsonProperty("unknown")
+        private String getValue() {
+            return value;
+        }
+
+        @Override
+        public <T> T accept(Visitor<T> visitor) {
+            return visitor.visitUnknown_(value);
+        }
+
+        @Override
+        public boolean equals(Object other) {
+            return this == other || (other instanceof Unknown_Wrapper && equalTo((Unknown_Wrapper) other));
+        }
+
+        private boolean equalTo(Unknown_Wrapper other) {
+            return this.value.equals(other.value);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hashCode(this.value);
+        }
+
+        @Override
+        public String toString() {
+            return "Unknown_Wrapper{value: " + value + '}';
+        }
+    }
+
+    @JsonTypeInfo(
+            use = JsonTypeInfo.Id.NAME,
+            include = JsonTypeInfo.As.EXISTING_PROPERTY,
+            property = "type",
+            visible = true)
+    private static final class UnknownWrapper implements Base {
+        private final String type;
+
+        private final Map<String, Object> value;
+
+        @JsonCreator
+        private UnknownWrapper(@JsonProperty("type") String type) {
+            this(type, new HashMap<String, Object>());
+        }
+
+        private UnknownWrapper(@Nonnull String type, @Nonnull Map<String, Object> value) {
+            Preconditions.checkNotNull(type, "type cannot be null");
+            Preconditions.checkNotNull(value, "value cannot be null");
+            this.type = type;
+            this.value = value;
+        }
+
+        @JsonProperty
+        private String getType() {
+            return type;
+        }
+
+        @JsonAnyGetter
+        private Map<String, Object> getValue() {
+            return value;
+        }
+
+        @JsonAnySetter
+        private void put(String key, Object val) {
+            value.put(key, val);
+        }
+
+        @Override
+        public <T> T accept(Visitor<T> visitor) {
+            return visitor.visitUnknown(type);
+        }
+
+        @Override
+        public boolean equals(Object other) {
+            return this == other || (other instanceof UnknownWrapper && equalTo((UnknownWrapper) other));
+        }
+
+        private boolean equalTo(UnknownWrapper other) {
+            return this.type.equals(other.type) && this.value.equals(other.value);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(this.type, this.value);
+        }
+
+        @Override
+        public String toString() {
+            return "UnknownWrapper{type: " + type + ", value: " + value + '}';
+        }
+    }
+}
