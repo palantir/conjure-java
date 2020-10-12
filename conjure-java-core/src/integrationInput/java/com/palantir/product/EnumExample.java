5d4a478cf682eabcfe7e5c56fa345d06e3715ca1
iamdanfox <iamdanfox@gmail.com>
2020-10-12 12:24:53 +0200

    Annotate enums with error-prone Immutable (#1086)
    The java classes for conjure enums are now annotated with `@com.google.errorprone.annotations.Immutable`, which ensures that if they're used inside hand-rolled enums, Google's [ImmutableEnumChecker](https://errorprone.info/bugpattern/ImmutableEnumChecker) won't complain.

--- conjure-java-core/src/integrationInput/java/com/palantir/product/EnumExample.java
+++ conjure-java-core/src/integrationInput/java/com/palantir/product/EnumExample.java
@@ -2,6 +2,7 @@ package com.palantir.product;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonValue;
+import com.google.errorprone.annotations.Immutable;
 import com.palantir.logsafe.Preconditions;
 import java.util.Arrays;
 import java.util.Collections;
@@ -23,6 +24,7 @@ import javax.annotation.Nonnull;
  * There is no method to access all instantiations of this class, since they cannot be known at compile time.
  */
 @Generated("com.palantir.conjure.java.types.EnumGenerator")
+@Immutable
 public final class EnumExample {
     public static final EnumExample ONE = new EnumExample(Value.ONE, "ONE");
 
