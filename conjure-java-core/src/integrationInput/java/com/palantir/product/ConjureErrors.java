71f03c9c896e954408d6f93631c3ce928e3adf0a
Kristof Farkas-Pall <kristofarkas@gmail.com>
2021-09-08 15:52:53 +0000

    Generate isError methods for conjure errors

--- conjure-java-core/src/integrationInput/java/com/palantir/product/ConjureErrors.java
+++ conjure-java-core/src/integrationInput/java/com/palantir/product/ConjureErrors.java
@@ -1,6 +1,7 @@
 package com.palantir.product;
 
 import com.palantir.conjure.java.api.errors.ErrorType;
+import com.palantir.conjure.java.api.errors.RemoteException;
 import com.palantir.conjure.java.api.errors.ServiceException;
 import com.palantir.logsafe.SafeArg;
 import com.palantir.logsafe.UnsafeArg;
@@ -77,5 +78,21 @@ public final class ConjureErrors {
         if (shouldThrow) {
             throw invalidTypeDefinition(typeName, typeDef);
         }
+    }
+
+    /**
+     * Returns true if the {@link RemoteException} is named Conjure:InvalidServiceDefinition
+     */
+    public static boolean isInvalidServiceDefinition(RemoteException remoteException) {
+        return INVALID_SERVICE_DEFINITION
+                .name()
+                .equals(remoteException.getError().errorName());
+    }
+
+    /**
+     * Returns true if the {@link RemoteException} is named Conjure:InvalidTypeDefinition
+     */
+    public static boolean isInvalidTypeDefinition(RemoteException remoteException) {
+        return INVALID_TYPE_DEFINITION.name().equals(remoteException.getError().errorName());
     }
 }
