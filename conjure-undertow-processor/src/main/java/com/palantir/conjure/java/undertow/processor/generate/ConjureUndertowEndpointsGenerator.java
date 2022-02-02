2f3b815b0f4e2f54e6f54a7cd10d46174e589df8
Fabian Windheuser <fwindheuser@palantir.com>
2022-02-02 19:36:08 +0000

    [undertow-annotations] Handle query params with primitive types (#1629)
    Handle query params with primitive types

--- conjure-undertow-processor/src/main/java/com/palantir/conjure/java/undertow/processor/generate/ConjureUndertowEndpointsGenerator.java
+++ conjure-undertow-processor/src/main/java/com/palantir/conjure/java/undertow/processor/generate/ConjureUndertowEndpointsGenerator.java
@@ -226,7 +226,7 @@ public final class ConjureUndertowEndpointsGenerator {
                 TypeName paramType = def.argType().match(ArgTypeTypeName.INSTANCE);
                 additionalFields.add(ImmutableAdditionalField.builder()
                         .field(FieldSpec.builder(
-                                        ParameterizedTypeName.get(ClassName.get(Deserializer.class), paramType),
+                                        ParameterizedTypeName.get(ClassName.get(Deserializer.class), paramType.box()),
                                         deserializerFieldName,
                                         Modifier.PRIVATE,
                                         Modifier.FINAL)
