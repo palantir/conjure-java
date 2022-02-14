60e2a4fa2c6ac60823273c72039cc5808af3b571
Seif Sameh <seifs@palantir.com>
2022-02-14 19:29:27 +0000

    add JsonIgnoreProperties annotation when strictObject=false for empty beans

---
+++ conjure-java-core/src/integrationInput/java/com/palantir/strict/EmptyObjectNotStrict.java
@@ -0,0 +1,25 @@
+package com.palantir.strict;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import javax.annotation.Generated;
+
+@JsonSerialize
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Generated("com.palantir.conjure.java.types.BeanGenerator")
+public final class EmptyObjectNotStrict {
+    private static final EmptyObjectNotStrict INSTANCE = new EmptyObjectNotStrict();
+
+    private EmptyObjectNotStrict() {}
+
+    @Override
+    public String toString() {
+        return "EmptyObjectNotStrict{}";
+    }
+
+    @JsonCreator(mode = JsonCreator.Mode.DELEGATING)
+    public static EmptyObjectNotStrict of() {
+        return INSTANCE;
+    }
+}
