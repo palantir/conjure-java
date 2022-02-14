5d62ac7230668fd4b020bd16eafb50db586e4b87
Fabian Windheuser <fwindheuser@palantir.com>
2022-02-14 20:06:20 +0000

    Revert "Overload encoding registry constructors"
    This reverts commit 116ac24cde784c9eb4021e3d8edc8f0c6e442e9d.

--- conjure-java-undertow-runtime/src/main/java/com/palantir/conjure/java/undertow/runtime/ConjureBodySerDe.java
+++ conjure-java-undertow-runtime/src/main/java/com/palantir/conjure/java/undertow/runtime/ConjureBodySerDe.java
@@ -67,12 +67,12 @@ final class ConjureBodySerDe implements BodySerDe {
 
     @Override
     public <T> Serializer<T> serializer(TypeMarker<T> token) {
-        return new EncodingSerializerRegistry<>(encodings, token);
+        return new EncodingSerializerRegistry<>(encodings, token, Optional.empty());
     }
 
     @Override
     public <T> Serializer<T> serializer(TypeMarker<T> token, Endpoint endpoint) {
-        return new EncodingSerializerRegistry<>(encodings, token, endpoint);
+        return new EncodingSerializerRegistry<>(encodings, token, Optional.of(endpoint));
     }
 
     @Override
@@ -82,7 +82,7 @@ final class ConjureBodySerDe implements BodySerDe {
 
     @Override
     public <T> Deserializer<T> deserializer(TypeMarker<T> token, Endpoint endpoint) {
-        return new EncodingDeserializerRegistry<>(encodings, token, endpoint);
+        return new EncodingDeserializerRegistry<>(encodings, token, Optional.of(endpoint));
     }
 
     @Override
@@ -112,15 +112,7 @@ final class ConjureBodySerDe implements BodySerDe {
         private final EncodingSerializerContainer<T> defaultEncoding;
         private final List<EncodingSerializerContainer<T>> encodings;
 
-        EncodingSerializerRegistry(List<Encoding> encodings, TypeMarker<T> token) {
-            this(encodings, token, Optional.empty());
-        }
-
-        EncodingSerializerRegistry(List<Encoding> encodings, TypeMarker<T> token, Endpoint endpoint) {
-            this(encodings, token, Optional.of(endpoint));
-        }
-
-        private EncodingSerializerRegistry(List<Encoding> encodings, TypeMarker<T> token, Optional<Endpoint> endpoint) {
+        EncodingSerializerRegistry(List<Encoding> encodings, TypeMarker<T> token, Optional<Endpoint> endpoint) {
             this.encodings = encodings.stream()
                     .map(encoding -> new EncodingSerializerContainer<>(encoding, token, endpoint))
                     .collect(ImmutableList.toImmutableList());
@@ -177,16 +169,7 @@ final class ConjureBodySerDe implements BodySerDe {
         private final boolean optionalType;
         private final TypeMarker<T> marker;
 
-        EncodingDeserializerRegistry(List<Encoding> encodings, TypeMarker<T> token) {
-            this(encodings, token, Optional.empty());
-        }
-
-        EncodingDeserializerRegistry(List<Encoding> encodings, TypeMarker<T> token, Endpoint endpoint) {
-            this(encodings, token, Optional.of(endpoint));
-        }
-
-        private EncodingDeserializerRegistry(
-                List<Encoding> encodings, TypeMarker<T> token, Optional<Endpoint> endpoint) {
+        EncodingDeserializerRegistry(List<Encoding> encodings, TypeMarker<T> token, Optional<Endpoint> endpoint) {
             this.encodings = encodings.stream()
                     .map(encoding -> new EncodingDeserializerContainer<>(encoding, token, endpoint))
                     .collect(ImmutableList.toImmutableList());
