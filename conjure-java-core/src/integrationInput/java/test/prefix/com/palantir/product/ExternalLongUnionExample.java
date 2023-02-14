5561f5ae37092dc4a41b9d4dc175477e043901d5
Lavanya Singh <lsingh@palantir.com>
2023-02-14 12:57:22 -0600

    bump the conjure parser and add integration tests that take advantage of safety annotations on external longs

---
+++ conjure-java-core/src/integrationInput/java/test/prefix/com/palantir/product/ExternalLongUnionExample.java
@@ -0,0 +1,268 @@
+package test.prefix.com.palantir.product;
+
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonSetter;
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.annotation.JsonTypeName;
+import com.fasterxml.jackson.annotation.JsonValue;
+import com.palantir.logsafe.Preconditions;
+import com.palantir.logsafe.Safe;
+import com.palantir.logsafe.SafeArg;
+import com.palantir.logsafe.exceptions.SafeIllegalArgumentException;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import javax.annotation.processing.Generated;
+
+/**
+ * A union of a safe long.
+ */
+@Generated("com.palantir.conjure.java.types.UnionGenerator")
+public final class ExternalLongUnionExample {
+    private final Base value;
+
+    @JsonCreator(mode = JsonCreator.Mode.DELEGATING)
+    private ExternalLongUnionExample(Base value) {
+        this.value = value;
+    }
+
+    @JsonValue
+    private Base getValue() {
+        return value;
+    }
+
+    public static ExternalLongUnionExample safeLong(long value) {
+        return new ExternalLongUnionExample(new SafeLongWrapper(value));
+    }
+
+    public static ExternalLongUnionExample unknown(@Safe String type, Object value) {
+        switch (Preconditions.checkNotNull(type, "Type is required")) {
+            case "safeLong":
+                throw new SafeIllegalArgumentException(
+                        "Unknown type cannot be created as the provided type is known: safeLong");
+            default:
+                return new ExternalLongUnionExample(new UnknownWrapper(type, Collections.singletonMap(type, value)));
+        }
+    }
+
+    public <T> T accept(Visitor<T> visitor) {
+        return value.accept(visitor);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return this == other
+                || (other instanceof ExternalLongUnionExample && equalTo((ExternalLongUnionExample) other));
+    }
+
+    private boolean equalTo(ExternalLongUnionExample other) {
+        return this.value.equals(other.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return this.value.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return "ExternalLongUnionExample{value: " + value + '}';
+    }
+
+    public interface Visitor<T> {
+        T visitSafeLong(long value);
+
+        T visitUnknown(@Safe String unknownType);
+
+        static <T> SafeLongStageVisitorBuilder<T> builder() {
+            return new VisitorBuilder<T>();
+        }
+    }
+
+    private static final class VisitorBuilder<T>
+            implements SafeLongStageVisitorBuilder<T>, UnknownStageVisitorBuilder<T>, Completed_StageVisitorBuilder<T> {
+        private Function<Long, T> safeLongVisitor;
+
+        private Function<String, T> unknownVisitor;
+
+        @Override
+        public UnknownStageVisitorBuilder<T> safeLong(@Nonnull Function<Long, T> safeLongVisitor) {
+            Preconditions.checkNotNull(safeLongVisitor, "safeLongVisitor cannot be null");
+            this.safeLongVisitor = safeLongVisitor;
+            return this;
+        }
+
+        @Override
+        public Completed_StageVisitorBuilder<T> unknown(@Nonnull Function<String, T> unknownVisitor) {
+            Preconditions.checkNotNull(unknownVisitor, "unknownVisitor cannot be null");
+            this.unknownVisitor = unknownVisitor;
+            return this;
+        }
+
+        @Override
+        public Completed_StageVisitorBuilder<T> throwOnUnknown() {
+            this.unknownVisitor = unknownType -> {
+                throw new SafeIllegalArgumentException(
+                        "Unknown variant of the 'ExternalLongUnionExample' union",
+                        SafeArg.of("unknownType", unknownType));
+            };
+            return this;
+        }
+
+        @Override
+        public Visitor<T> build() {
+            final Function<Long, T> safeLongVisitor = this.safeLongVisitor;
+            final Function<String, T> unknownVisitor = this.unknownVisitor;
+            return new Visitor<T>() {
+                @Override
+                public T visitSafeLong(long value) {
+                    return safeLongVisitor.apply(value);
+                }
+
+                @Override
+                public T visitUnknown(String value) {
+                    return unknownVisitor.apply(value);
+                }
+            };
+        }
+    }
+
+    public interface SafeLongStageVisitorBuilder<T> {
+        UnknownStageVisitorBuilder<T> safeLong(@Nonnull Function<Long, T> safeLongVisitor);
+    }
+
+    public interface UnknownStageVisitorBuilder<T> {
+        Completed_StageVisitorBuilder<T> unknown(@Nonnull Function<String, T> unknownVisitor);
+
+        Completed_StageVisitorBuilder<T> throwOnUnknown();
+    }
+
+    public interface Completed_StageVisitorBuilder<T> {
+        Visitor<T> build();
+    }
+
+    @JsonTypeInfo(
+            use = JsonTypeInfo.Id.NAME,
+            include = JsonTypeInfo.As.EXISTING_PROPERTY,
+            property = "type",
+            visible = true,
+            defaultImpl = UnknownWrapper.class)
+    @JsonSubTypes(@JsonSubTypes.Type(SafeLongWrapper.class))
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    private interface Base {
+        <T> T accept(Visitor<T> visitor);
+    }
+
+    @JsonTypeName("safeLong")
+    private static final class SafeLongWrapper implements Base {
+        private final long value;
+
+        @JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
+        private SafeLongWrapper(@JsonSetter("safeLong") @Nonnull long value) {
+            Preconditions.checkNotNull(value, "safeLong cannot be null");
+            this.value = value;
+        }
+
+        @JsonProperty(value = "type", index = 0)
+        private String getType() {
+            return "safeLong";
+        }
+
+        @JsonProperty("safeLong")
+        private long getValue() {
+            return value;
+        }
+
+        @Override
+        public <T> T accept(Visitor<T> visitor) {
+            return visitor.visitSafeLong(value);
+        }
+
+        @Override
+        public boolean equals(@Nullable Object other) {
+            return this == other || (other instanceof SafeLongWrapper && equalTo((SafeLongWrapper) other));
+        }
+
+        private boolean equalTo(SafeLongWrapper other) {
+            return this.value == other.value;
+        }
+
+        @Override
+        public int hashCode() {
+            return Long.hashCode(this.value);
+        }
+
+        @Override
+        public String toString() {
+            return "SafeLongWrapper{value: " + value + '}';
+        }
+    }
+
+    private static final class UnknownWrapper implements Base {
+        private final String type;
+
+        private final Map<String, Object> value;
+
+        @JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
+        private UnknownWrapper(@JsonProperty("type") String type) {
+            this(type, new HashMap<String, Object>());
+        }
+
+        private UnknownWrapper(@Nonnull String type, @Nonnull Map<String, Object> value) {
+            Preconditions.checkNotNull(type, "type cannot be null");
+            Preconditions.checkNotNull(value, "value cannot be null");
+            this.type = type;
+            this.value = value;
+        }
+
+        @JsonProperty
+        private String getType() {
+            return type;
+        }
+
+        @JsonAnyGetter
+        private Map<String, Object> getValue() {
+            return value;
+        }
+
+        @JsonAnySetter
+        private void put(String key, Object val) {
+            value.put(key, val);
+        }
+
+        @Override
+        public <T> T accept(Visitor<T> visitor) {
+            return visitor.visitUnknown(type);
+        }
+
+        @Override
+        public boolean equals(@Nullable Object other) {
+            return this == other || (other instanceof UnknownWrapper && equalTo((UnknownWrapper) other));
+        }
+
+        private boolean equalTo(UnknownWrapper other) {
+            return this.type.equals(other.type) && this.value.equals(other.value);
+        }
+
+        @Override
+        public int hashCode() {
+            int hash = 1;
+            hash = 31 * hash + this.type.hashCode();
+            hash = 31 * hash + this.value.hashCode();
+            return hash;
+        }
+
+        @Override
+        public String toString() {
+            return "UnknownWrapper{type: " + type + ", value: " + value + '}';
+        }
+    }
+}
