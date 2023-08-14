033d3b3aecc764fcec54486c5257f08269a95408
Patrick Koenig <pkoenig10@gmail.com>
2023-08-14 14:03:24 -0700

    Add StdSerializer and StdDeserializer (#2080)

---
+++ conjure-undertow-annotations/src/main/java/com/palantir/conjure/java/undertow/annotations/StdSerializer.java
@@ -0,0 +1,32 @@
+/*
+ * (c) Copyright 2021 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.conjure.java.undertow.annotations;
+
+import com.palantir.conjure.java.undertow.lib.Endpoint;
+import com.palantir.conjure.java.undertow.lib.Serializer;
+import com.palantir.conjure.java.undertow.lib.TypeMarker;
+import com.palantir.conjure.java.undertow.lib.UndertowRuntime;
+
+@SuppressWarnings({"RawTypes", "unchecked"})
+public interface StdSerializer<T> extends SerializerFactory<T>, Serializer<T> {
+
+    @Override
+    default <T1 extends T> Serializer<T1> serializer(
+            TypeMarker<T1> _type, UndertowRuntime _runtime, Endpoint _endpoint) {
+        return (Serializer<T1>) this;
+    }
+}
