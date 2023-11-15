07f6b6e06e98eeae4751d8228fba53f8c525064d
Brandon Marc-Aurele <bmarcaurele@palantir.com>
2023-11-15 09:58:18 -0500

    move this call back to async

--- conjure-java-core/src/test/java/com/palantir/conjure/java/UndertowServiceEteTest.java
+++ conjure-java-core/src/test/java/com/palantir/conjure/java/UndertowServiceEteTest.java
@@ -45,6 +45,7 @@ import com.palantir.dialogue.BinaryRequestBody;
 import com.palantir.dialogue.clients.DialogueClients;
 import com.palantir.product.EmptyPathService;
 import com.palantir.product.EmptyPathServiceEndpoints;
+import com.palantir.product.EteBinaryServiceAsync;
 import com.palantir.product.EteBinaryServiceBlocking;
 import com.palantir.product.EteBinaryServiceEndpoints;
 import com.palantir.product.EteServiceAsync;
@@ -102,11 +103,13 @@ public final class UndertowServiceEteTest extends TestBase {
     private final EteServiceAsync asyncClient;
 
     private final EteBinaryServiceBlocking binaryClient;
+    private final EteBinaryServiceAsync asyncBinaryClient;
 
     public UndertowServiceEteTest() {
         this.client = DialogueClients.create(EteServiceBlocking.class, clientConfiguration());
         this.asyncClient = DialogueClients.create(EteServiceAsync.class, clientConfiguration());
         this.binaryClient = DialogueClients.create(EteBinaryServiceBlocking.class, clientConfiguration());
+        this.asyncBinaryClient = DialogueClients.create(EteBinaryServiceAsync.class, clientConfiguration());
     }
 
     @BeforeAll
@@ -462,17 +465,23 @@ public final class UndertowServiceEteTest extends TestBase {
     public void testBinaryServerSideFailureAfterFewBytesReceived() {
         int chunkSize = 1024;
         int expectedChunks = 1024; // 1 MB
-        int chunksToSend = 1024 * 64; // 64 MB
+        int chunksToSend = 1024 * 1024; // 1 GB
         int bytesExpected = expectedChunks * chunkSize;
 
         byte[] data = new byte[chunkSize];
         ThreadLocalRandom.current().nextBytes(data);
-        assertThatThrownBy(() -> binaryClient.postBinaryThrows(
-                        AuthHeader.valueOf("authHeader"), bytesExpected, (OutputStream sink) -> {
-                            for (int i = 0; i < chunksToSend; i++) {
-                                sink.write(data);
-                            }
-                        }))
+        assertThatThrownBy(() -> {
+                    try {
+                        Futures.getUnchecked(asyncBinaryClient.postBinaryThrows(
+                                AuthHeader.valueOf("authHeader"), bytesExpected, (OutputStream sink) -> {
+                                    for (int i = 0; i < chunksToSend; i++) {
+                                        sink.write(data);
+                                    }
+                                }));
+                    } catch (UncheckedExecutionException e) {
+                        throw e.getCause();
+                    }
+                })
                 .isInstanceOfSatisfying(
                         RemoteException.class, re -> assertThat(re.getStatus()).isEqualTo(400));
     }
