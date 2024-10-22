a1f407a6c6d78ed331fbe1cc3ac840ae77296d38
Brandon Marc-Aurele <bmarcaurele@palantir.com>
2024-10-22 13:23:22 -0400

    efficient primitive item addition

--- conjure-lib/src/main/java/com/palantir/conjure/java/lib/internal/ConjureCollections.java
+++ conjure-lib/src/main/java/com/palantir/conjure/java/lib/internal/ConjureCollections.java
@@ -170,8 +170,12 @@ public final class ConjureCollections {
 
     // This method modifies a list that can't handle nulls. Do not use this unless the nonNullCollections flag is set
     public static void addAllToDoubleList(Collection<Double> addTo, double[] elementsToAdd) {
-        for (double el : elementsToAdd) {
-            addTo.add(el);
+        if (addTo instanceof ConjureDoubleList) {
+            ((ConjureDoubleList) addTo).addAll(elementsToAdd);
+        } else {
+            for (double el : elementsToAdd) {
+                addTo.add(el);
+            }
         }
     }
 
@@ -195,8 +199,12 @@ public final class ConjureCollections {
 
     // This method modifies a list that can't handle nulls. Do not use this unless the nonNullCollections flag is set
     public static void addAllToIntegerList(Collection<Integer> addTo, int[] elementsToAdd) {
-        for (int el : elementsToAdd) {
-            addTo.add(el);
+        if (addTo instanceof ConjureIntegerList) {
+            ((ConjureIntegerList) addTo).addAll(elementsToAdd);
+        } else {
+            for (int el : elementsToAdd) {
+                addTo.add(el);
+            }
         }
     }
 
