1509be088ab9c504e891aae5b5f98475399ffde6
Pritham Marupaka <pmarupaka@palantir.com>
2025-09-10 17:43:03 -0400

    update dialogue version

--- conjure-java-core/src/test/java/com/palantir/conjure/java/UndertowServiceEteTest.java
+++ conjure-java-core/src/test/java/com/palantir/conjure/java/UndertowServiceEteTest.java
@@ -588,7 +588,7 @@ public final class UndertowServiceEteTest extends TestBase {
             // .getError() returns the SerializableError which should contain the legacy parameters sent over the wire.
             jsonParams = e.getError().parameters();
             // e should be an instance of ErrorWithComplexArgsException, which has rich parameters as well.
-            //            assertThat(e.getCause()).isInstanceOfSatisfying(ErrorWithComplexArgsException.class, exception
+            //            assertThat(e).isInstanceOfSatisfying(ErrorWithComplexArgsException.class, exception
             // -> {
             //                assertThat(exception.error().parameters().optionalExample().getOptionalString())
             //                        .contains("optional-value");
