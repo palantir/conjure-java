5a8bc7b13627808175e68095aa275aeb03e41103
Kunal Kak <kkak@palantir.com>
2025-11-06 20:14:46 -0500

    add camel case example and failing test

---
+++ conjure-java-core/src/integrationInput/java/sealedunions/com/palantir/product/CamelCaseUnion.java
@@ -0,0 +1,249 @@
+package sealedunions.com.palantir.product;
+
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonSetter;
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.annotation.JsonTypeName;
+import com.palantir.logsafe.Preconditions;
+import com.palantir.logsafe.Safe;
+import com.palantir.logsafe.SafeArg;
+import com.palantir.logsafe.exceptions.SafeIllegalArgumentException;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import javax.annotation.processing.Generated;
+
+@Generated("com.palantir.conjure.java.types.UnionGenerator")
+@JsonTypeInfo(
+        use = JsonTypeInfo.Id.NAME,
+        include = JsonTypeInfo.As.EXISTING_PROPERTY,
+        property = "type",
+        visible = true,
+        defaultImpl = CamelCaseUnion.Unknown.class)
+@JsonSubTypes(@JsonSubTypes.Type(value = CamelCaseUnion.CamelCasedField.class, name = "camelcasedfield"))
+@JsonIgnoreProperties(ignoreUnknown = true)
+public abstract sealed class CamelCaseUnion permits CamelCaseUnion.CamelCasedField, CamelCaseUnion.Unknown {
+    public static CamelCaseUnion camelCasedField(String value) {
+        return new CamelCasedField(value);
+    }
+
+    public static CamelCaseUnion unknown(@Safe String type, Object value) {
+        switch (Preconditions.checkNotNull(type, "Type is required")) {
+            case "camelCasedField":
+                throw new SafeIllegalArgumentException(
+                        "Unknown type cannot be created as the provided type is known: camelCasedField");
+            default:
+                return new Unknown(type, Collections.singletonMap(type, value));
+        }
+    }
+
+    public Known throwOnUnknown() {
+        if (this instanceof Unknown) {
+            throw new SafeIllegalArgumentException(
+                    "Unknown variant of the 'CamelCaseUnion' union",
+                    SafeArg.of("unknownType", ((Unknown) this).type()));
+        } else {
+            return (Known) this;
+        }
+    }
+
+    public abstract <T> T accept(Visitor<T> visitor);
+
+    public sealed interface Known permits CamelCasedField {}
+
+    @JsonTypeName("camelCasedField")
+    public static final class CamelCasedField extends CamelCaseUnion implements Known {
+        private final String value;
+
+        @JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
+        private CamelCasedField(@JsonSetter("camelCasedField") @Nonnull String value) {
+            Preconditions.checkNotNull(value, "camelCasedField cannot be null");
+            this.value = value;
+        }
+
+        @JsonProperty(index = 0)
+        private String type() {
+            return "camelCasedField";
+        }
+
+        @JsonProperty("camelCasedField")
+        public String value() {
+            return value;
+        }
+
+        @Override
+        public <T> T accept(Visitor<T> visitor) {
+            return visitor.visitCamelCasedField(value);
+        }
+
+        @Override
+        public boolean equals(@Nullable Object other) {
+            return this == other || (other instanceof CamelCasedField && equalTo((CamelCasedField) other));
+        }
+
+        private boolean equalTo(CamelCasedField other) {
+            return this.value.equals(other.value);
+        }
+
+        @Override
+        public int hashCode() {
+            return this.value.hashCode();
+        }
+
+        @Override
+        public String toString() {
+            return "CamelCaseUnion.CamelCasedField{value: " + value + '}';
+        }
+    }
+
+    public static final class Unknown extends CamelCaseUnion {
+        private final String type;
+
+        private final Map<String, Object> value;
+
+        @JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
+        private Unknown(@JsonProperty("type") String type) {
+            this(type, new HashMap<String, Object>());
+        }
+
+        private Unknown(@Nonnull String type, @Nonnull Map<String, Object> value) {
+            Preconditions.checkNotNull(type, "type cannot be null");
+            Preconditions.checkNotNull(value, "value cannot be null");
+            this.type = type;
+            this.value = value;
+        }
+
+        @JsonProperty
+        public String type() {
+            return type;
+        }
+
+        @JsonAnyGetter
+        public Map<String, Object> value() {
+            return value;
+        }
+
+        @JsonAnySetter
+        private void put(String key, Object val) {
+            value.put(key, val);
+        }
+
+        @Override
+        public <T> T accept(Visitor<T> visitor) {
+            return visitor.visitUnknown(type, value.get(type));
+        }
+
+        @Override
+        public boolean equals(@Nullable Object other) {
+            return this == other || (other instanceof Unknown && equalTo((Unknown) other));
+        }
+
+        private boolean equalTo(Unknown other) {
+            return this.type.equals(other.type) && this.value.equals(other.value);
+        }
+
+        @Override
+        public int hashCode() {
+            int hash = 1;
+            hash = 31 * hash + this.type.hashCode();
+            hash = 31 * hash + this.value.hashCode();
+            return hash;
+        }
+
+        @Override
+        public String toString() {
+            return "CamelCaseUnion.Unknown{type: " + type + ", value: " + value + '}';
+        }
+    }
+
+    public interface Visitor<T> {
+        T visitCamelCasedField(String value);
+
+        T visitUnknown(@Safe String unknownType, Object unknownValue);
+
+        static <T> CamelCasedFieldStageVisitorBuilder<T> builder() {
+            return new VisitorBuilder<T>();
+        }
+    }
+
+    private static final class VisitorBuilder<T>
+            implements CamelCasedFieldStageVisitorBuilder<T>,
+                    UnknownStageVisitorBuilder<T>,
+                    Completed_StageVisitorBuilder<T> {
+        private Function<String, T> camelCasedFieldVisitor;
+
+        private BiFunction<@Safe String, Object, T> unknownVisitor;
+
+        @Override
+        public UnknownStageVisitorBuilder<T> camelCasedField(@Nonnull Function<String, T> camelCasedFieldVisitor) {
+            Preconditions.checkNotNull(camelCasedFieldVisitor, "camelCasedFieldVisitor cannot be null");
+            this.camelCasedFieldVisitor = camelCasedFieldVisitor;
+            return this;
+        }
+
+        @Override
+        public Completed_StageVisitorBuilder<T> unknown(@Nonnull BiFunction<@Safe String, Object, T> unknownVisitor) {
+            Preconditions.checkNotNull(unknownVisitor, "unknownVisitor cannot be null");
+            this.unknownVisitor = unknownVisitor;
+            return this;
+        }
+
+        @Override
+        public Completed_StageVisitorBuilder<T> unknown(@Nonnull Function<@Safe String, T> unknownVisitor) {
+            Preconditions.checkNotNull(unknownVisitor, "unknownVisitor cannot be null");
+            this.unknownVisitor = (unknownType, _unknownValue) -> unknownVisitor.apply(unknownType);
+            return this;
+        }
+
+        @Override
+        public Completed_StageVisitorBuilder<T> throwOnUnknown() {
+            this.unknownVisitor = (unknownType, _unknownValue) -> {
+                throw new SafeIllegalArgumentException(
+                        "Unknown variant of the 'CamelCaseUnion' union", SafeArg.of("unknownType", unknownType));
+            };
+            return this;
+        }
+
+        @Override
+        public Visitor<T> build() {
+            final Function<String, T> camelCasedFieldVisitor = this.camelCasedFieldVisitor;
+            final BiFunction<@Safe String, Object, T> unknownVisitor = this.unknownVisitor;
+            return new Visitor<T>() {
+                @Override
+                public T visitCamelCasedField(String value) {
+                    return camelCasedFieldVisitor.apply(value);
+                }
+
+                @Override
+                public T visitUnknown(String unknownType, Object unknownValue) {
+                    return unknownVisitor.apply(unknownType, unknownValue);
+                }
+            };
+        }
+    }
+
+    public interface CamelCasedFieldStageVisitorBuilder<T> {
+        UnknownStageVisitorBuilder<T> camelCasedField(@Nonnull Function<String, T> camelCasedFieldVisitor);
+    }
+
+    public interface UnknownStageVisitorBuilder<T> {
+        Completed_StageVisitorBuilder<T> unknown(@Nonnull BiFunction<@Safe String, Object, T> unknownVisitor);
+
+        Completed_StageVisitorBuilder<T> unknown(@Nonnull Function<@Safe String, T> unknownVisitor);
+
+        Completed_StageVisitorBuilder<T> throwOnUnknown();
+    }
+
+    public interface Completed_StageVisitorBuilder<T> {
+        Visitor<T> build();
+    }
+}
